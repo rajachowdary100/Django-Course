33d7c63349f6e095fd8e5b5580ac262157874156
rajachowdary100 <rajachowdary.polepalli99@outlook.com>
2022-12-13 21:42:10 +0530

    modified the code!

--- DJango/Django_sample.py
+++ DJango/Django_sample.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = sns.load_dataset("iris")
+#print("Hello World!")
+print(df)
+print(df.shape)
+print(df.columns)
+print(df.info())
+for x in ['sepal_length', 'sepal_width', 'petal_length', 'petal_width','species']:
+    sns.histplot(df[x])
+    plt.show()
+
+
+
