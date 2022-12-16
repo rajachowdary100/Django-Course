e128ccdf60cd49c3cf9e1d45a77bab513eb866b2
rajachowdary100 <rajachowdary.polepalli99@outlook.com>
2022-12-16 11:41:43 +0530

    modifed the code!

--- DJango/Django_sample.py
+++ DJango/Django_sample.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("pandas to spark").getOrCreate()
 
 df = sns.load_dataset("iris")
 #print("Hello World!")
@@ -9,9 +11,10 @@ print(df)
 print(df.shape)
 print(df.columns)
 print(df.info())
-for x in ['sepal_length', 'sepal_width', 'petal_length', 'petal_width','species']:
-    sns.histplot(df[x])
-    plt.show()
+#for x in ['sepal_length', 'sepal_width', 'petal_length', 'petal_width','species']:
+#    sns.histplot(df[x])
+#    plt.show()
 
-
+df_spark = spark.createDataFrame(df,schema=df.columns.tolist())
+print(df_spark.show())
 
